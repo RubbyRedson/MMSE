55b4f60741d4e331605ab0ec9b2c0f7c227fece1
RubbyRedson <gurnik2009@gmail.com>
2016-10-15 22:42:30 +0200

    Complete unit test for Client API

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -22,11 +22,64 @@ class ClientTest extends TestCase
      */
     public function testGetOne()
     {
-        $this->assertEquals(
-            "{\"id\":1,\"name\":\"Pear LLC\",\"phone\":\"123321123\",\"discount\":0,\"created_at\":null,\"updated_at\":null}",
-            $this->getWithAuth('/api/client/1')
-        );
+        $response = $this->getWithAuth('/api/client/1');
+        $jsonArr = json_decode($response, true);
+
+        $this->assertEquals($jsonArr['name'], 'Pear LLC 3');
+        $this->assertEquals($jsonArr['phone'], '123321123');
+        $this->assertEquals($jsonArr['discount'], '0');
     }
 
+    /**
+     * Test to create new client.
+     *
+     * @return void
+     */
+    public function testCreateNew()
+    {
+        $json = "{\"name\":\"Pear LLC 4\",\"phone\":\"1233211233\",\"discount\":1}";
+        $response = $this->postWithAuth('/api/client', $json);
+        $jsonArr = json_decode($response, true);
 
+        $this->assertEquals($jsonArr['name'], 'Pear LLC 4');
+        $this->assertEquals($jsonArr['phone'], '1233211233');
+        $this->assertEquals($jsonArr['discount'], '1');
+    }
+
+    /**
+     * Test to update existing client.
+     *
+     * @return void
+     */
+    public function testUpdateExisting()
+    {
+        $json1 = "{\"name\":\"Pear LLC 25\",\"phone\":\"123321123\",\"discount\":1}";
+        $response1 = $this->postWithAuth('/api/client', $json1);
+        $jsonArr1 = json_decode($response1, true);
+        $id = $jsonArr1['id'];
+
+        $json = "{\"name\":\"Pear LLC 31\",\"phone\":\"1233211233\",\"discount\":0}";
+        $response = $this->putWithAuth('/api/client/'.$id, $json);
+        $jsonArr = json_decode($response, true);
+
+        $this->assertEquals($jsonArr['name'], 'Pear LLC 31');
+        $this->assertEquals($jsonArr['phone'], '1233211233');
+        $this->assertEquals($jsonArr['discount'], '0');
+    }
+    /**
+     * Test to delete existing client.
+     *
+     * @return void
+     */
+    public function testDeleteExisting()
+    {
+        $json1 = "{\"name\":\"Pear LLC 25\",\"phone\":\"123321123\",\"discount\":1}";
+        $response1 = $this->postWithAuth('/api/client', $json1);
+        $jsonArr1 = json_decode($response1, true);
+        $id = $jsonArr1['id'];
+
+        $response = $this->deleteWithAuth('/api/client/'.$id);
+
+        $this->assertEquals('"success"', $response);
+    }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -18,11 +18,41 @@ class TestCase extends Laravel\Lumen\Testing\TestCase
         return $response->getContent();
     }
 
+    protected function postWithAuth($uri, $content)
+    {
+        $response = $this->call('POST', $uri, array(), array(), array(),
+            $this->transformHeadersToServerVars(['Authorization' => $this->getToken(), 'CONTENT_TYPE' => 'application/json']),
+            $content);
+        return $response->getContent();
+    }
+
+    protected function putWithAuth($uri, $content)
+    {
+        $response = $this->call('PUT', $uri, array(), array(), array(),
+            $this->transformHeadersToServerVars(['Authorization' => $this->getToken(), 'CONTENT_TYPE' => 'application/json']),
+            $content);
+        return $response->getContent();
+    }
+
+    protected function deleteWithAuth($uri)
+    {
+        $response = $this->call('DELETE', $uri, array(), array(), array(),
+            $this->transformHeadersToServerVars(['Authorization' => $this->getToken()]));
+        return $response->getContent();
+    }
+
     protected function getToken()
     {
         $json = '{"username":"Alice","password":"abc123"}';
         $tokenResponse = $this->call('POST', '/api/user/login', array(), array(), array(), ['CONTENT_TYPE' => 'application/json'], $json)->content();
         $token = json_decode($tokenResponse, true)['token'];
         return $token;
+    }
+
+    protected function getNow()
+    {
+        $newExpiry = new \DateTime();
+        $newExpiry->setTimestamp((new \DateTime())->getTimestamp() + 3600);
+        return $newExpiry->format('Y-m-d H:i:s');
     }
 }
